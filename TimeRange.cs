3e3510ed085c60167f8c9c08d9db65bb1b23b9f8
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-01-21 14:21:17 +0530

    TimeRangeAssignment

---
+++ TimeRange.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intersection
+{
+    class TimeRange
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public TimeRange(DateTime start, DateTime end)
+        {
+            this.StartDate = start;
+            this.EndDate = end;
+        }
+
+        public TimeRange()
+        {
+
+        }
+    }
+}
+
+
+
+
